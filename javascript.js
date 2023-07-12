ba8f428a68aa5395ae943f9ee4bbafd3e81ef598
fionicss <118982867+Fionicss@users.noreply.github.com>
2023-07-12 22:08:45 +0800

    Rearranged javascript file for better organization and added end screen upon reaching 5 points.

--- javascript.js
+++ javascript.js
@@ -1,39 +1,45 @@
 let computerSelection = getComputerChoice();
-        console.log(computerSelection);
-
 let playerSelection = "";
-        
 let result = "";
-
 let playerPoints = 0;
-
 let computerPoints = 0;
+let resultText = document.querySelector(".resultText");
+let wins = document.querySelector(".winsNum");
+let losses = document.querySelector(".lossesNum");
+let wholeContainer = document.querySelector(".whole");
+let btnContainer = document.querySelector(".button");
 
 const paper = document.querySelector(".Paper");
     paper.addEventListener('click', () => {
     playerSelection = 'paper';
     showResults ();
     chooseAgain ();
+    gameOver ();
 })
         
 const scissors = document.querySelector(".Scissors");
-    scissors.addEventListener('click', () => {
-    playerSelection = 'scissors';
+    scissors.addEventListener("click", () => {
+    playerSelection = "scissors";
     showResults ();
     chooseAgain ();
+    gameOver ();
 })
         
 const rock = document.querySelector(".Rock");
-    rock.addEventListener('click', () => {
-    playerSelection = 'rock';
+    rock.addEventListener("click", () => {
+    playerSelection = "rock";
     showResults ();
     chooseAgain ();
+    gameOver ();
 })
 // On clicking the button player selection will be set to its respective choice and playRound will play via showResults.;
 
-let resultText = document.querySelector(".resultText")
-let wins = document.querySelector(".winsNum")
-let losses = document.querySelector(".lossesNum")
+const tryAgain = document.createElement ("button");
+tryAgain.addEventListener ("click", () => {
+    window.location.reload ()
+})
+// Reloads page upon clicking
+
 
 function getComputerChoice () {
     const options = ["rock", "paper", "scissors"];
@@ -42,9 +48,32 @@ function getComputerChoice () {
     return chosen;
     }
 
-// function addScore {
-
-// }    
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "rock" || playerSelection == "paper" && computerSelection == "paper"
+        || playerSelection == "scissors" && computerSelection == "scissors") {
+        result = "Tie!";
+        tie ();
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        result = "You win! Rock beats Scissors";
+        win ();
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        result = "You win! Paper beats Rock";
+        win ();
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        result = "You win! Scissors beats Paper";
+        win ();
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        result = "You lose! Paper beats Rock";
+        lose ();
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        result = "You lose! Scissors beats Paper";
+        lose ();
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        result = "You lose! Rock beats Scissors";
+        lose ();
+    } else {
+        result = "Try again!";
+    }}
 
 function win () {
     playerPoints += 1;
@@ -68,42 +97,12 @@ function tie () {
     resultText.textContent = `${result}`;
     wins.textContent = `${playerPoints}`;
     losses.textContent = `${computerPoints}`;
-
 }
 
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "rock" || playerSelection == "paper" && computerSelection == "paper"
-        || playerSelection == "scissors" && computerSelection == "scissors") {
-            result = "Tie!";
-            tie ();
-        } else if (playerSelection == "rock" && computerSelection == "scissors") {
-            result = "You win! Rock beats Scissors";
-            win ();
-        }else if (playerSelection == "paper" && computerSelection == "rock") {
-            result = "You win! Paper beats Rock";
-            win ();
-        } else if (playerSelection == "scissors" && computerSelection == "paper") {
-            result = "You win! Scissors beats Paper";
-            win ();
-        } else if (playerSelection == "rock" && computerSelection == "paper") {
-            result = "You lose! Paper beats Rock";
-            lose ();
-        } else if (playerSelection == "paper" && computerSelection == "scissors") {
-            result = "You lose! Scissors beats Paper";
-            lose ();
-        } else if (playerSelection == "scissors" && computerSelection == "rock") {
-            result = "You lose! Rock beats Scissors";
-            lose ();
-        } else {
-            result = "Try again!";
-}}
-
 function showResults () {
-    console.log (playerSelection);
     playRound (playerSelection, computerSelection);
-    console.log (result);
-    console.log (playerPoints)
-    console.log (computerPoints)
+    console.log (playerPoints);
+    console.log (computerPoints);
 }
 
 function chooseAgain () {
@@ -111,3 +110,19 @@ function chooseAgain () {
     console.log(computerSelection);
     //resets the computer choice to play another round;
 }
+
+function gameOver () {
+    if (playerPoints == 5) {
+    wholeContainer.removeChild(btnContainer);
+    wholeContainer.insertBefore(tryAgain, resultText)
+    tryAgain.textContent = "Play Again!"
+    result = "You win!";
+    resultText.textContent = `${result}`;
+} else if (computerPoints == 5) {
+    wholeContainer.removeChild(btnContainer);
+    wholeContainer.insertBefore(tryAgain, resultText)
+    tryAgain.textContent = "Play Again!"
+    result = "You Lose!";
+    resultText.textContent = `${result}`;
+}}
+// Checks if points have reached 5 and adds the play again button.
